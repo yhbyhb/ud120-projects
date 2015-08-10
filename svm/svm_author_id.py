b80136a07a20afb9b501e073e4133c38df632b9a
HanByul Yang <hanbyul.yang@gmail.com>
2015-08-10 18:27:29 +0900

    Lesson 2 - mini project

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -26,7 +26,33 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+from sklearn.svm import SVC
 
+# Optimize C Parameter, Optimized RBF vs Linear SVM: Accuracy
+clf = SVC(C=10000.0, kernel="rbf")
+
+# A Smaller Training Set
+# features_train = features_train[:len(features_train)/100] 
+# labels_train = labels_train[:len(labels_train)/100] 
+
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+
+t0 = time()
+pred = clf.predict(features_test)
+print "predicting time:", round(time()-t0, 3), "s"
+
+from sklearn.metrics import accuracy_score
+acc = accuracy_score(pred, labels_test)
+print "accuracy :", round(acc, 4)
+
+# Extracting Predictions from an SVM
+# print pred[10], pred[26], pred[50]
+
+# How Many Chris Emails Predicted?
+# import numpy as np
+# print np.count_nonzero(pred)
 #########################################################
 
 
