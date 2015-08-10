69f1e32522964a89eae83bd8c2fac7c864b9d371
HanByul Yang <hanbyul.yang@gmail.com>
2015-08-10 18:27:22 +0900

    Lesson 1 - mini project

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -28,6 +28,25 @@ features_train, features_test, labels_train, labels_test = preprocess()
 #########################################################
 ### your code goes here ###
 
+### import the sklearn module for GaussianNB
+from sklearn.naive_bayes import GaussianNB
+
+### create classifier
+clf = GaussianNB()
+
+### fit the classifier on the training features and labels
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+
+### use the trained classifier to predict labels for the test features
+t1 = time()
+pred = clf.predict(features_test)
+print "predicting time:", round(time()-t1, 3), "s"
+
+from sklearn.metrics import accuracy_score
+accuracy = accuracy_score(labels_test, pred)
+print "accuracy :", round(accuracy, 4)
 
 #########################################################
 
