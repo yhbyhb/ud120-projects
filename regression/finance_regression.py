a9f73887a6cc8d7d27bd85fa8e5f290104c3ef67
HanByul Yang <hanbyul.yang@gmail.com>
2015-09-02 20:36:22 +0900

    Lesson 6 - mini project, Sneak Peek: Outliers Break Regressions

--- regression/finance_regression.py
+++ regression/finance_regression.py
@@ -22,7 +22,7 @@ dictionary = pickle.load( open("../final_project/final_project_dataset_modified.
 
 ### list the features you want to look at--first item in the 
 ### list will be the "target" feature
-features_list = ["bonus", "long_term_incentive"]
+features_list = ["bonus", "salary"]
 data = featureFormat( dictionary, features_list, remove_any_zeroes=True)
 target, features = targetFeatureSplit( data )
 
@@ -66,6 +66,11 @@ try:
     plt.plot( feature_test, reg.predict(feature_test) )
 except NameError:
     pass
+
+reg.fit(feature_test, target_test)
+plt.plot(feature_train, reg.predict(feature_train), color="b") 
+print 'new slope :', reg.coef_
+
 plt.xlabel(features_list[1])
 plt.ylabel(features_list[0])
 plt.legend()
