5660eaf127dbddde4e64bde283b3d1d79253c836
HanByul Yang <hanbyul.yang@gmail.com>
2015-09-04 01:20:39 +0900

    Lesson 7 - mini project, Any More Outliers?

--- outliers/enron_outliers.py
+++ outliers/enron_outliers.py
@@ -9,6 +9,7 @@ from feature_format import featureFormat, targetFeatureSplit
 
 ### read in data dictionary, convert to numpy array
 data_dict = pickle.load( open("../final_project/final_project_dataset.pkl", "r") )
+data_dict.pop('TOTAL', 0)
 features = ["salary", "bonus"]
 data = featureFormat(data_dict, features)
 
