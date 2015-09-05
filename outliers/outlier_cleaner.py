59bb4d0045d2157e092f61678be5377b6ae3da5a
HanByul Yang <hanbyul.yang@gmail.com>
2015-09-05 15:41:07 +0900

    Lesson 7 - mini project, Score After Cleaning

--- outliers/outlier_cleaner.py
+++ outliers/outlier_cleaner.py
@@ -14,7 +14,18 @@ def outlierCleaner(predictions, ages, net_worths):
     cleaned_data = []
 
     ### your code goes here
+    error_dict = {}
+    for i in range(0, len(ages)):
+        error = abs(predictions[i][0] - net_worths[i][0])
+        error_dict[i] = error
 
-    
+    import operator
+    sorted_error = sorted(error_dict.items(), key=operator.itemgetter(1))
+
+    for i in range(0, int(round(len(ages) * 0.9))):
+        index = sorted_error[i][0]
+        cleaned_item = (ages[index][0], net_worths[index][0], sorted_error[i][1])
+        cleaned_data.append(cleaned_item)
+
     return cleaned_data
 
