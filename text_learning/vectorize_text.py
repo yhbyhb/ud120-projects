dae0dc28041cbda6511e80150cbd0c76e391bd3f
HanByul Yang <hanbyul.yang@gmail.com>
2015-09-06 16:45:46 +0900

    Lesson 10 - mini project, Signature Scrubbing

--- text_learning/vectorize_text.py
+++ text_learning/vectorize_text.py
@@ -47,14 +47,21 @@ for name, from_person in [("sara", from_sara), ("chris", from_chris)]:
             email = open(path, "r")
 
             ### use parseOutText to extract the text from the opened email
+            words = parseOutText(email)
 
             ### use str.replace() to remove any instances of the words
             ### ["sara", "shackleton", "chris", "germani"]
+            for discard in ["sara", "shackleton", "chris", "germani"]:
+                words = words.replace(discard, "")
 
             ### append the text to word_data
+            word_data.append(words)
 
             ### append a 0 to from_data if email is from Sara, and 1 if email is from Chris
-
+            if name == "sara":
+                from_data.append(0)
+            elif name =="chris":
+                from_data.append(1)
 
             email.close()
 
@@ -65,8 +72,7 @@ from_chris.close()
 pickle.dump( word_data, open("your_word_data.pkl", "w") )
 pickle.dump( from_data, open("your_email_authors.pkl", "w") )
 
-
-
+print word_data[152]
 
 
 ### in Part 4, do TfIdf vectorization here
