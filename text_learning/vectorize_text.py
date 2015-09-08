31fda69793994cb1e52edf7a6567e81751bcb25d
HanByul Yang <hanbyul.yang@gmail.com>
2015-09-09 01:31:07 +0900

    Lesson 11 - mini project, Remove, Repeat

--- text_learning/vectorize_text.py
+++ text_learning/vectorize_text.py
@@ -51,7 +51,7 @@ for name, from_person in [("sara", from_sara), ("chris", from_chris)]:
 
         ### use str.replace() to remove any instances of the words
         ### ["sara", "shackleton", "chris", "germani"]
-        for discard in ["sara", "shackleton", "chris", "germani", "sshacklensf"]:
+        for discard in ["sara", "shackleton", "chris", "germani", "sshacklensf", "cgermannsf"]:
             words = words.replace(discard, "")
 
         ### append the text to word_data
