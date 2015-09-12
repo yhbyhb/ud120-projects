22081d481f1f6a0b5ab824912b92d5a10c11c380
HanByul Yang <hanbyul.yang@gmail.com>
2015-09-12 16:52:08 +0900

    Lesson 13 - mini project, Baby's First Correct POI ID

--- validation/validate_poi.py
+++ validation/validate_poi.py
@@ -29,14 +29,14 @@ labels, features = targetFeatureSplit(data)
 
 ### it's all yours from here forward!
 from sklearn import cross_validation
-features_train, features_test, labels_train, labels_test = cross_validation.train_test_split(features, labels)
+features_train, features_test, labels_train, labels_test = cross_validation.train_test_split(features, labels, test_size=0.3, random_state=42)
 
 from sklearn import tree
 clf = tree.DecisionTreeClassifier()
-clf = clf.fit(features, labels)
-pred = clf.predict(features)
+clf = clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
 from sklearn.metrics import accuracy_score
-acc = accuracy_score(pred, labels)
+acc = accuracy_score(pred, labels_test)
 print "accuracy :", acc
 
 
